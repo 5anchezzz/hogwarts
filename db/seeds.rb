a251b7e4f84fb8e1842f5beaa3d36a05133e684f
5anchezzz <5an4e3.free@gmail.com>
2020-02-09 22:14:38 +0300

    use faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+8.times do
+  house = House.create(name: Faker::Movies::HarryPotter.unique.house,
+                       points: rand(10..90))
+
+  num_of_students = rand(10..20)
+  num_of_teachers = rand(4..8)
+
+  num_of_students.times do
+    house.students.create(name: Faker::Movies::HarryPotter.unique.character)
+  end
+
+  num_of_teachers.times do
+    house.teachers.create(name: Faker::Movies::HarryPotter.unique.character)
+  end
+
+end
+
+puts "Number of houses: #{House.count}"
+puts "Number of students: #{Student.count}"
+puts "Number of teachers: #{Teacher.count}"
